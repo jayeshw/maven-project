715d1ae2f18a6b0eaf46e81294954df7ad723e40
Wairale <JWairale@us.imshealth.com>
2018-07-09 13:09:30 -0700

    JSon response with test case.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,6 +3,7 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -12,9 +13,15 @@ public class HelloMavenTest extends FunctionalTestCase {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
+    
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
     }
-
 }
